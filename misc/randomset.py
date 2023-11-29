18b30d4b034547207f290ee15d7bacc1cd5c6d8a
PointBreaker <475669397@qq.com>
2023-11-29 12:37:23 +0800

    add randomset

---
+++ misc/randomset.py
@@ -0,0 +1,15 @@
+class RandomSet:
+    '''
+    Insert, delete, getRandom all in O(1)
+    '''
+    def __init__(self):
+        pass
+    
+    def insert(val):
+        pass
+    
+    def delete(val):
+        pass
+    
+    def getRandom():
+        pass
