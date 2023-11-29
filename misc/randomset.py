51d4066298eb9bd2b4f0dc1967081912d0133326
PointBreaker <475669397@qq.com>
2023-11-29 21:16:47 +0800

    implemented randomset

--- misc/randomset.py
+++ misc/randomset.py
@@ -1,15 +1,42 @@
+from random import choice
 class RandomSet:
     '''
     Insert, delete, getRandom all in O(1)
     '''
     def __init__(self):
-        pass
+        self.index_map = {}
+        self.list_of_values = []
+        self.last_val = -1
     
-    def insert(val):
-        pass
+    def insert(self, val):
+        if val in self.index_map.keys():
+            return True
+        self.list_of_values.append(val)
+        self.index_map[val] = self.last_val
+        return False
     
-    def delete(val):
-        pass
+    def delete(self, val): 
+        if val not in self.index_map.keys():
+            return False
+        else: # swap the value to be deleted with last element, and then pop it
+            self.last_val -= 1
+            last_val, index = self.list_of_values[-1], self.index_map[val]
+            self.list_of_values[index] = val
+            self.index_map[last_val] = index
+            self.list_of_values.pop()
+            return True
     
-    def getRandom():
-        pass
+    def getRandom(self):
+        return choice(self.list_of_values)
+    
+if __name__ == "__main__":
+    s = RandomSet()
+    print(s.insert(1))
+    print(s.delete(2))
+    print(s.insert(2))
+    print(s.insert(3))
+    print(s.getRandom())
+    print(s.delete(3))
+    print(s.getRandom())
+    print(s.delete(2))
+    print(s.getRandom())
